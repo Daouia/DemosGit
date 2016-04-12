7cab51fd6747f6ff1c772120eb64ab9cc80aaf72
unknown <Stagiaire@R203-9.demostraining.com>
2016-04-12 09:32:27 +0200

    Ajout de caracteres

--- src/Syntaxe.java
+++ src/Syntaxe.java
@@ -3,7 +3,7 @@ public class Syntaxe {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Maj("toTTToJJJJITITUITUIhnjhjhhrjghhgurhur"));
+		System.out.println(Maj("toTTToJJJJITITUITUIhnjhjhhrjghhgurhuraa"));
 
 		
 	}
