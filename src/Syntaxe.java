bd0361c5489d5c8c7f812ca147c56ccf50bc8806
unknown <Stagiaire@R203-9.demostraining.com>
2016-04-12 09:23:29 +0200

    premier commit

---
+++ src/Syntaxe.java
@@ -0,0 +1,21 @@
+
+public class Syntaxe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Maj("toTTToJJJJITITUITUIhnjhjhhrjghhgurhur"));
+
+		
+	}
+	public static String Maj(String mot){
+		return mot.substring(0, 1).toUpperCase()+mot.substring(1).toLowerCase();
+		
+					
+		}
+	
+	}
+
+	
+	
+
+
